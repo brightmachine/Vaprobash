a5d99c886e265a0e22e2c0dd55f399fbe3827847
Kelvin Jones <kelvinj@gmail.com>
2014-02-10 22:02:55 +0000

    startup script to restart apache after /vagrant mounted

--- scripts/apache_vagrant_vhosts.sh
+++ scripts/apache_vagrant_vhosts.sh
@@ -9,3 +9,17 @@ sudo a2enconf vhosts-vagrant
 sudo a2enmod vhost_alias
 
 sudo service apache2 restart
+
+# On boot apache is started before /vagrant is mounted, therefore after vagrant is mounted we should
+# restart apache again. This upstart script does just that.
+cat > /etc/init/apache-vagrant-vhosts.conf << EOF
+# restart apache after vagrant mounted
+description "restart apache after vagrant mounted"
+author "Kelvin Jones"
+
+# Listen and start after the vagrant-mounted event
+start on vagrant-mounted
+
+exec /etc/init.d/apache2 restart
+
+EOF
