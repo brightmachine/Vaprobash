286608749a320a77db0d8da856b259e9b5c2a4bf
Kelvin Jones <kelvinj@gmail.com>
2014-05-01 14:03:05 +0100

    purge old php install as it bails on modified php.ini file
    …which has been resolved

--- scripts/php.sh
+++ scripts/php.sh
@@ -21,7 +21,8 @@ fi
 sudo apt-get update
 
 # Install PHP
-rm /etc/php5/fpm/php.ini
+#mv /etc/php5/fpm/php.ini.ucf-dist /etc/php5/fpm/php.ini
+sudo apt-get purge --force-yes -y php5-cli php5-fpm
 sudo apt-get install --force-yes -y php5-cli php5-fpm php5-mysql php5-pgsql php5-sqlite php5-curl php5-gd php5-gmp php5-mcrypt php5-xdebug php5-memcached php5-imagick
 
 # xdebug Config
